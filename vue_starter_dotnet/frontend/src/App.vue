269cf0f1354e09f238fba9d612bd8865acb12106
Basal Ali <basalali@hotmail.com>
2019-12-10 13:55:41 -0500

    Vue mvp done

--- vue_starter_dotnet/frontend/src/App.vue
+++ vue_starter_dotnet/frontend/src/App.vue
@@ -1,8 +1,28 @@
+
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view/>
+    <ChatBot msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
+
+<script>
+import ChatBot from './components/Chatbot.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ChatBot
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

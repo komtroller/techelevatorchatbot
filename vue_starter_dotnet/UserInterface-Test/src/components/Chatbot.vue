35889bff58acdbaf94a1b95ce2fc34bd3803a760
Basal Ali <basalali@hotmail.com>
2019-12-16 15:38:22 -0500

    vuejs update

--- vue_starter_dotnet/UserInterface-Test/src/components/Chatbot.vue
+++ vue_starter_dotnet/UserInterface-Test/src/components/Chatbot.vue
@@ -1,10 +1,8 @@
 <template>
 
 <section class="chat-box">
-  <img class="techyimg" src= "../Logo.png">
-<div class="title">{{name}} 
-</div>
-<div class="info">{{info}}</div> 
+ <br/><br/><br/>
+
 <div class="chat-box-list-container" ref="chatbox">
 <ul class="chat-box-list">
 
@@ -23,7 +21,7 @@
   </ul>
 </div>
 <div class="chat-inputs">
-<input id='chat-input' type="text" maxlength='100' placeholder='Type a message' v-model="message" @keyup.enter="sendMessage"/>
+<input type="text" maxlength='100' placeholder='Type a message...' v-model="message" @keyup.enter="sendMessage"/>
 <button @click="sendMessage">Send</button>
 </div>
 </section>
@@ -34,13 +32,12 @@
 import { createWriteStream } from 'fs';
 export default {
 name: 'Chatbot',
+blank: ' fasfsf',
 
 data: () => ({
 
 message: '',
 messages: [{text:"Hello, What is your name?", author: "server"}],
-name: "Chattio",
-info: "Tech Elevator Student Bot"
 }),
 methods: {
 sendMessage() {
@@ -58,12 +55,7 @@ let query = this.message
     author: 'client'
   })
   this.message = ''
-  // this.$axios.get(`https://localhost:44392/api/chat/${message}`)
-  // .then(res => {
-  //   this.messages.push({
-  //     text: res.data.output,
-  //     author: 'server'
-  //   })
+
   let arrayLength = this.messages.length;
   console.log(arrayLength);
   if (arrayLength < 3)
@@ -101,30 +93,7 @@ let query = this.message
 
 <style scoped lang="scss">
 
-.techyimg{
-  width:60px;
-}
-
-.info {
-  font-display: flex;
-  font-size: 20px;
-   text-align: center;
-
-
-}
-
-.title {
- text-align: center;
-font-family: 'Arial';
-font-size: 30px;
-font-weight: bold;
-transition: {
-property: font-size;
-duration: 4s;
-delay: 2s;
-}
-}
-
+// this styling displays the presentation of the messages
 .chat-box,
 .chat-box-list {
 background-color: white !important;
@@ -133,39 +102,47 @@ flex-direction: column;
 list-style-type: none;
 word-wrap: break-word;
 }
+//////////////// Scroll functionality of the
 .chat-box-list-container {
 background-color: white !important;
 overflow: scroll;
 margin-bottom: 1px;
 }
+
+// This styling displays the look of the messages;
+
 .chat-box-list {
 padding-left: 10px;
 padding-right: 10px;
 span {
-padding: 8px;
+padding: 1.5px;
 color: white;
-border-radius: 4px;
+border-radius: 6px;
 }
 .server {
-span {
-background: #99cc00;
-box-shadow: 3px 3px 3px green;
-}
-p {
-float: right;
-}
+    span {
+        background: #0c8d51;
+        box-shadow: 4px 4px 4px rgba(8, 95, 8, 0.486);
+         }
+     p  {
+          float: right;
+        }
 }
 .client {
-span {
-background: #0070C8;
-box-shadow: 3px 3px 3px blue;
+     span {
+         background: #0070C8;
+         box-shadow: 4px 4px 4px blue;
 
+        }
+      p {
+        float: left;
+        }
+      }
 }
-p {
-float: left;
-}
-}
-}
+//////////////////////////
+
+// This style formats the overall Chatbox
+
 .chat-box {
 
 margin: 10px;
@@ -178,30 +155,33 @@ margin-right: auto;
 align-items: space-between;
 justify-content: space-between;
 }
+
+// This styling formats the layout of the input-box and send button
+
 .chat-inputs {
 display: flex;
 
 
-input {
-line-height: 3;
-width: 100%;
-border: 1px solid #999;
-border-left: none;
-border-bottom: none;
-border-right: none;
-border-bottom-left-radius: 4px;
-padding-left: 15px;
-}
-button {
-width: 145px;
-color: white;
-background: rgb(185, 204, 18);
-border-color: #999;
-border-bottom: none;
-border-right:none;
-border-bottom-right-radius: 4px;
-}
+      input {
+      line-height: 3;
+      width: 100%;
+      border: 1px solid #999;
+      border-left: none;
+      border-bottom: none;
+      border-right: none;
+      border-bottom-left-radius: 4px;
+      padding-left: 15px;
+      }
+       button {
+       width: 145px;
+       color: white;
+       background: #0c8d51;
+       border-color: #999;
+       border-bottom: none;
+       border-right:none;
+       border-bottom-right-radius: 4px;
+       border-left:none;
+      }
 }
 
 </style>
-
